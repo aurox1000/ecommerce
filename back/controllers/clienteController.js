70aa8009be19db427cb2d01f8633ecb24ff3e417
Vicente Cortes <vicente@criteria.mx>
2023-08-26 11:04:08 -0600

    Estructura

--- back/controllers/clienteController.js
+++ back/controllers/clienteController.js
@@ -1,10 +1,7 @@
-const testing = async function(req,res)
-{
+const testing = async function(req, res)
+{}
 
-    res.status(200).send('hola Testin3');
-}
+module.export = {
+testing
 
-module.exports = 
-{
-    testing
-}
+} 
--- back/models/usuario.js
+++ back/models/usuario.js
@@ -5,6 +5,9 @@ nombres :  {type : String, required: true},
 apellidos: {type: String, required: true},
 email:     {type: String, required: true, unique: true},
 rol:       {type: String, required: true},
-password:   {type: String, required: true}
+password:  {type: String, required: true},
+estatus:   {type: Boolean, default: true, require: true},
+createdAt: {type: Date, default: Date.now}
+
 });
 module.exports = mongosse.model('usuario', UsuarioSchema);
